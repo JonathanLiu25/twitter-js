fb388e8c15dbaafa5b132ac31d02f16b9d3e25a8
ekotwick <edmond.kotwick@gmail.com>
2017-03-07 15:10:41 -0500

    up to sockets

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,14 @@ const router = express.Router();
 const tweetBank = require('../tweetBank');
 const bodyParser = require('body-parser');
 
+
+
+// router.use(express.bodyParser());
+
+router.use(bodyParser.urlencoded({ extended: false }))
+router.use(bodyParser.json());
+
+
 router.use(express.static('public'));
 
 router.get( '/tweets/:id', function(request, response){
@@ -12,14 +20,23 @@ router.get( '/tweets/:id', function(request, response){
 });
 
 router.get( '/users/:name', function(request, response){
-	var person = request.params.name;
-	var tweets = tweetBank.find( {name: person} );
-	response.render( 'index', {tweets: tweets} );
+	var name = request.params.name;
+	var tweets = tweetBank.find( {name: name} );
+	response.render( 'index', {tweets: tweets, showForm: true, name: name} );
 });
 
 router.get('/', function(request, response){
 	var tweets = tweetBank.list();
-	response.render( 'index', {tweets: tweets} );
+	response.render( 'index', {tweets: tweets, showForm: true} );
+});
+
+router.post('/tweets', function(request, response) {
+
+  var name = request.body.name;
+  var text = request.body.text;
+  tweetBank.add(name, text);
+
+  response.redirect('/');
 });
 
 module.exports = router;
